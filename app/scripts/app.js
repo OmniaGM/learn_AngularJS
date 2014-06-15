1060b201ab13eb0d85e0ee880328497128052612
Omnia Gamal <omnia.gamal1988@gmail.com>
2014-06-15 15:00:25 +0300

    Step 0

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,10 @@
 (function(){
   'use strict';
 
-  angular.module('readingListApp', ['readingList'])
+  angular.module('readingList', [])
 
-  .controller('ReadingListController', ['$http','$scope', function($http, $scope){
-      var store = this;
-      store.books = [];
-      $http.get('data/books.json').success(function(data){
-        store.books = $scope.books = data;
-      });
-      $http.get('data/genres.json').success(function(data){
-        store.genres = $scope.genres = data;
-      });
+  .controller('ReadingListController', function(){
+    alert('welcom');
+  })
 
-      this.showForm = false;
-    }])
-
-  
 })();
